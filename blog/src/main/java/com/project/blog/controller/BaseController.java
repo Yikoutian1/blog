32223dd1a68e2185e36df79af0db1b212581c99f
Your Name <you@example.com>
2023-01-12 21:38:26 +0800

    添加代码生成器（AutoGenerator）

---
+++ blog/src/main/java/com/project/blog/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.project.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName BaseController
+ * @Description TODO
+ * @Author QiuLiHang
+ * @DATE 2023/1/12 21:34
+ * @Version 1.0
+ */
+
+@RestController
+public class BaseController {
+
+
+}
---
+++ blog/src/main/java/com/project/blog/entity/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.project.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName BaseEntity
+ * @Description TODO
+ * @Author QiuLiHang
+ * @DATE 2023/1/12 21:35
+ * @Version 1.0
+ */
+
+@Data
+public class BaseEntity implements Serializable {
+
+    @ApiModelProperty("id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty("创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @ApiModelProperty("更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @TableField(value = "update_time", fill = FieldFill.UPDATE)
+    private Date updateTime;
+
+    @ApiModelProperty("状态 已封禁/已删除/正常")
+    @TableField("status")
+    private Integer status;
+}
